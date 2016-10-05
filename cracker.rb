a0a3c12c22f08666ba65cd397cce1cf14249e632
MarFerPra <marco.fp94@gmail.com>
2016-10-05 23:32:21 +0200

    Calculo de frecuencias y adivinar permutaciones en cada subcadena.

--- cracker.rb
+++ cracker.rb
@@ -59,9 +59,11 @@ def encontrar_clave(criptograma)
 end
 
 def descifrar(cadena)
+  alfabeto = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
   cadena_ary = cadena.split("")
   unique_chars = cadena_ary.uniq
   aparicion_letras = {}
+  distancia_e = 0
 
   for char in unique_chars do
     apariciones_count = cadena.count(char)
@@ -69,7 +71,33 @@ def descifrar(cadena)
     aparicion_letras[char] = porcentaje_aparicion.round(4)
   end
   aparicion_letras = aparicion_letras.sort_by {|k,v| v}.reverse
-  puts aparicion_letras.to_s
+
+  if(aparicion_letras[0][1] > 13) # letra E
+    puts "Letra E: " + aparicion_letras[0][0] + " " + aparicion_letras[0][1].to_s
+    letra = aparicion_letras[0][0]
+    indice_letra = alfabeto.index(letra)
+    indice_e = alfabeto.index('E')
+    puts "Indice letra: " + indice_letra.to_s + " Indice E: " + (indice_e).to_s
+
+    if(indice_letra < indice_e)
+      distancia_e = (indice_e - indice_letra)
+    else
+      distancia_e = (indice_letra - indice_e)*(-1)
+    end
+
+  end
+
+  if(aparicion_letras[1][1] > 10 && aparicion_letras[1][1] < 13)
+    puts "Letra A: " + aparicion_letras[1][0] + " " + aparicion_letras[1][1].to_s
+
+    letra = aparicion_letras[1][0]
+    indice_letra = alfabeto.index(letra)
+    puts "Distancia E: " + distancia_e.to_s
+    if(alfabeto[indice_letra + distancia_e] == 'A')
+      puts "- CORRECTO"
+    end
+  end
+  puts "---------"
 end
 
 def chunk(string, size)
@@ -84,7 +112,6 @@ def main
   clave_final = 8 #salida[:clave]
 
   elementos_columna = (criptograma.length*1.0/clave_final*1.0).ceil
-  puts "Elementos columna: " + elementos_columna.to_s
 
   subcadenas_aux = chunk(criptograma, elementos_columna)
   subcadenas_fin = []
@@ -100,7 +127,9 @@ def main
   for cadena in subcadenas_fin do
       subcadenas_texto_plano << descifrar(cadena)
   end
- 
+
+
+
 end
 
 main
